cab613763816a466080ddd83245320305de76bff
jake-hong <hongse21@gmail.com>
2022-06-08 22:52:22 +0900

    fix serializer.py

--- TodoApp/todo/serializers.py
+++ TodoApp/todo/serializers.py
@@ -5,5 +5,4 @@ from .models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'compelted', 'pinned')
-
+        fields = ('id', 'title', 'completed', 'pinned')
